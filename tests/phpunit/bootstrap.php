32b98ab2ad56ae3348ce159e943777b4f2810e18
Soner Tari <sonertari@gmail.com>
2018-05-25 22:43:44 +0300

    Clean up

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -34,19 +34,6 @@ $TEST_DIR_SRC= $TEST_DIR . '/var/www/htdocs/pfre';
 /// @todo Is it better to use exec('whoami')?
 $INSTALL_USER= posix_getpwuid(posix_getuid())['name'];
 
-/// @todo Delete these after fixing NOTICEs
-// Forward compatibility
-//if (class_exists('\PHPUnit\Framework\Error\Warning') && !class_exists('\PHPUnit_Framework_Error_Warning')) {
-//	class_alias('\PHPUnit\Framework\Error\Warning', '\PHPUnit_Framework_Error_Warning');
-//}
-//
-//if (class_exists('\PHPUnit\Framework\Error\Notice') && !class_exists('\PHPUnit_Framework_Error_Notice')) {
-//	class_alias('\PHPUnit\Framework\Error\Notice', '\PHPUnit_Framework_Error_Notice');
-//}
-//
-//PHPUnit_Framework_Error_Warning::$enabled = FALSE;
-//PHPUnit_Framework_Error_Notice::$enabled = FALSE;
-
 // Forward compatibility
 if (class_exists('\PHPUnit\Framework\TestCase') && !class_exists('\PHPUnit_Framework_TestCase')) {
 	class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
